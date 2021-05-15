b522953837b3b3d396dbeb93cffd4bfe75c6b973
Kevin <hello@kevinroehl.com>
2021-05-15 19:16:58 +0200

    generate answers 2

--- src/Screens/Quiz/FatSecretQuiz.js
+++ src/Screens/Quiz/FatSecretQuiz.js
@@ -42,16 +42,41 @@ const FatSecretQuiz = props => {
     }, 1500);
   }
 
+  function randomizer(value) {
+    const ArraySmall = [0.2, 0.3, 0.5, 1.2, 1.4, 1.6, 2, 2.6, 3];
+    const ArrayBig = [0.2, 0.3, 0.7, 3, 4, 6];
+
+    if (value > 8) {
+      const shuffledArray = shuffle(ArraySmall);
+      return shuffledArray.slice(0, 3);
+    } else if (value <= 8) {
+      const shuffledArray = shuffle(ArrayBig);
+      return shuffledArray.slice(0, 3);
+    }
+  }
+
   function load() {
     if (fsRecipeIds.current !== null) {
       getRecipeDetails(fsRecipeIds[current]).then(recipe => {
         setRecipeDetails(recipe.recipe);
-
+        const randomValueArray = randomizer(recipe.recipe.serving_sizes.serving.carbohydrate);
         const array = [
-          {id: 1, value: randomValues(recipe.recipe.serving_sizes.serving.carbohydrate), right: false},
-          {id: 2, value: recipe.recipe.serving_sizes.serving.carbohydrate, right: true},
-          {id: 3, value: randomValues(recipe.recipe.serving_sizes.serving.carbohydrate), right: false},
-          {id: 4, value: randomValues(recipe.recipe.serving_sizes.serving.carbohydrate), right: false},
+          {
+            id: 1,
+            value: (randomValueArray[0] * recipe.recipe.serving_sizes.serving.carbohydrate).toFixed(0),
+            right: false,
+          },
+          {
+            id: 2,
+            value: (randomValueArray[1] * recipe.recipe.serving_sizes.serving.carbohydrate).toFixed(0),
+            right: false,
+          },
+          {
+            id: 3,
+            value: (randomValueArray[2] * recipe.recipe.serving_sizes.serving.carbohydrate).toFixed(0),
+            right: false,
+          },
+          {id: 4, value: parseInt(recipe.recipe.serving_sizes.serving.carbohydrate), right: true},
         ];
         setAnswers(prev => shuffle(array));
         setValidated(false);
