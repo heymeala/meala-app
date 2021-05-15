e23322399392095606539d325c9b077be5240641
Kevin <hello@kevinroehl.com>
2021-05-15 20:17:19 +0200

    styling

--- src/Screens/Quiz/FatSecretQuiz.js
+++ src/Screens/Quiz/FatSecretQuiz.js
@@ -4,9 +4,10 @@ import {Button, Image, makeStyles, Text} from 'react-native-elements';
 import {getRecipeDetails} from '../../Common/fatsecret/fatsecretApi';
 import LocalizationContext from '../../../LanguageContext';
 import {shuffle} from '../../utils/shuffel';
+import {serving} from '../../utils/specialTranslations';
 
 const FatSecretQuiz = props => {
-  const {t} = React.useContext(LocalizationContext);
+  const {t, locale} = React.useContext(LocalizationContext);
   const styles = useStyles();
   const {fsRecipeIds} = props;
   const [recipeDetails, setRecipeDetails] = useState(null);
@@ -44,7 +45,8 @@ const FatSecretQuiz = props => {
 
   function randomizer(value) {
     const ArraySmall = [0.2, 0.3, 0.5, 1.2, 1.4, 1.6, 2, 2.6, 3];
-    const ArrayBig = [0.2, 0.3, 0.7, 3, 4, 6];
+    const ArrayBig = [0, 0.7, 3, 4, 6, 7];
+
 
     if (value > 8) {
       const shuffledArray = shuffle(ArraySmall);
@@ -59,6 +61,7 @@ const FatSecretQuiz = props => {
     if (fsRecipeIds.current !== null) {
       getRecipeDetails(fsRecipeIds[current]).then(recipe => {
         setRecipeDetails(recipe.recipe);
+        console.log(recipe.recipe);
         const randomValueArray = randomizer(recipe.recipe.serving_sizes.serving.carbohydrate);
         const array = [
           {
@@ -93,7 +96,7 @@ const FatSecretQuiz = props => {
   }
 
   return recipeDetails !== null ? (
-    <View>
+    <View style={styles.container}>
       <Image
         style={styles.image}
         source={{
@@ -102,9 +105,10 @@ const FatSecretQuiz = props => {
             : recipeDetails.recipe_images.recipe_image,
         }}
       />
-      <Text h2>
-        <Text h2>{recipeDetails.serving_sizes.serving.serving_size}</Text> {recipeDetails.recipe_name}
-      </Text>
+      <View>
+        <Text h2>{recipeDetails.recipe_name}</Text>
+        <Text>{serving(locale, recipeDetails.serving_sizes.serving.serving_size)}</Text>
+      </View>
       {recipeDetails.serving_sizes && (
         <View style={styles.desc}>
           {answers &&
@@ -114,29 +118,41 @@ const FatSecretQuiz = props => {
                 key={i}
                 style={styles.answerButton}
                 onPress={() => validate(answer.right)}
-                title={answer.value + 'g'}
+                title={
+                  <>
+                    <Text h2>{answer.value}</Text>
+                    <Text>g</Text>
+                  </>
+                }
               />
             ))}
         </View>
       )}
-      <Text h2>{t('Recipes.ingredients')}</Text>
-      <Text>Portionen: {recipeDetails.number_of_servings}</Text>
-
+      <View style={styles.verticalSpacing}>
+        <Text h2>{t('Recipes.ingredients')}</Text>
+        <Text>für insgesamt {recipeDetails.number_of_servings} Portionen</Text>
+      </View>
       {recipeDetails.ingredients &&
         recipeDetails.ingredients.ingredient.map((ingre, i) => (
           <View style={styles.desc} key={i}>
-            <Text>{ingre.food_name}</Text>
-            <Text>
-              {ingre.number_of_units} {ingre.measurement_description}
-            </Text>
+            <Text h4>- {ingre.ingredient_description}</Text>
           </View>
         ))}
-      <Text h2>{t('Recipes.directions')}</Text>
+      <Text h2 style={styles.verticalSpacing}>
+        {t('Recipes.directions')}
+      </Text>
 
       {recipeDetails.directions && Array.isArray(recipeDetails.directions.direction)
         ? recipeDetails.directions.direction.map((desc, i) => (
             <View style={styles.desc} key={i}>
-              <Text>{desc.direction_description}</Text>
+              <View style={styles.descContainer}>
+                <View>
+                  <Text>{desc.direction_number}. </Text>
+                </View>
+                <View>
+                  <Text>{desc.direction_description}</Text>
+                </View>
+              </View>
             </View>
           ))
         : null}
@@ -148,7 +164,10 @@ export default FatSecretQuiz;
 
 const useStyles = makeStyles(theme => ({
   root: {minHeight: 700},
-  image: {width: '100%', height: 250, borderRadius: 10, marginBottom: theme.spacing.L},
+  container: {padding: theme.spacing.M},
+  image: {width: '100%', height: 250, borderRadius: 5, marginBottom: theme.spacing.L},
   desc: {paddingVertical: theme.spacing.S},
+  descContainer: {display: 'flex', flexDirection: 'row', paddingRight: 30},
   answerButton: {padding: theme.spacing.S},
+  verticalSpacing: {paddingVertical: theme.spacing.M},
 }));
