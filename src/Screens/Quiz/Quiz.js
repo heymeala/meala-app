34bf11b744df66cc882a29240dcd514d569820a3
Kevin <hello@kevinroehl.com>
2021-05-17 19:02:08 +0200

    quiz styling

--- src/Screens/Quiz/Quiz.js
+++ src/Screens/Quiz/Quiz.js
@@ -1,10 +1,13 @@
 import React, {useRef, useState} from 'react';
-import {SafeAreaView, ScrollView} from 'react-native';
-import {ListItem, makeStyles} from 'react-native-elements';
+import {SafeAreaView, ScrollView, View} from 'react-native';
+import {Badge, Divider, Icon, ListItem, makeStyles, Text} from 'react-native-elements';
 import LocalizationContext from '../../../LanguageContext';
 import FatSecretQuiz from './FatSecretQuiz';
 import {shuffle} from '../../utils/shuffel';
 import {quizServings} from './loadQuestionRecipes';
+import {badgeValue} from '../../Components/badgeValue';
+import {gradientPercentageColor, textColor} from '../../Common/generateColor';
+import CategoryListItem from './CategoryListItem';
 
 const Quiz = props => {
   const {t, locale} = React.useContext(LocalizationContext);
@@ -77,21 +80,40 @@ const Quiz = props => {
   return (
     <SafeAreaView>
       <ScrollView style={styles.root}>
-        <ListItem onPress={() => setQuizType(quizServings.carbohydrate)}>
-          <ListItem.Title>{t('Quiz.carbohydrates_game')}</ListItem.Title>
-        </ListItem>
-        <ListItem onPress={() => setQuizType(quizServings.calories)}>
-          <ListItem.Title>{t('Quiz.calories_game')}</ListItem.Title>
-        </ListItem>
-        <ListItem onPress={() => setQuizType(quizServings.fat)}>
-          <ListItem.Title>{t('Quiz.fat_game')}</ListItem.Title>
-        </ListItem>
-        <ListItem onPress={() => setQuizType(quizServings.protein)}>
-          <ListItem.Title>{t('Quiz.protein_game')}</ListItem.Title>
-        </ListItem>
-        <ListItem onPress={() => setQuizType(quizServings.fpe)}>
-          <ListItem.Title>{t('Quiz.fpe_game')}</ListItem.Title>
-        </ListItem>
+        <View style={styles.container}>
+          <Text h2>{t('Quiz.guess_question')}</Text>
+          <Text>{t('Quiz.guess_subtitle')}</Text>
+        </View>
+        <CategoryListItem
+          setQuizType={setQuizType}
+          quizServings={quizServings.carbohydrate}
+          title={t('Quiz.carbohydrates_game')}
+          subtitle={''}
+        />
+        <CategoryListItem
+          setQuizType={setQuizType}
+          quizServings={quizServings.calories}
+          title={t('Quiz.calories_game')}
+          subtitle={''}
+        />
+        <CategoryListItem
+          setQuizType={setQuizType}
+          quizServings={quizServings.fat}
+          title={t('Quiz.fat_game')}
+          subtitle={''}
+        />
+        <CategoryListItem
+          setQuizType={setQuizType}
+          quizServings={quizServings.protein}
+          title={t('Quiz.protein_game')}
+          subtitle={''}
+        />
+        <CategoryListItem
+          setQuizType={setQuizType}
+          quizServings={quizServings.fpe}
+          title={t('Quiz.fpe_game')}
+          subtitle={''}
+        />
       </ScrollView>
     </SafeAreaView>
   );
@@ -99,4 +121,6 @@ const Quiz = props => {
 
 export default Quiz;
 
-const useStyles = makeStyles(theme => ({}));
+const useStyles = makeStyles(theme => ({
+  container: {padding: theme.spacing.M},
+}));
