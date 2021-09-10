0450bcb9be73f82bffdfc6d80b3f56816e83c01f
Kevin <hello@kevinroehl.com>
2021-09-10 16:54:19 +0200

    news section

--- src/Screens/MealEntries/MealSearchController.js
+++ src/Screens/MealEntries/MealSearchController.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import LocalizationContext from '../../../LanguageContext';
 import { database } from '../../Common/database_realm';
-import { makeStyles } from 'react-native-elements';
+import { Button, makeStyles, Text } from 'react-native-elements';
 import SegmentedControlTab from 'react-native-segmented-control-tab';
 import DateList from './DateList';
 import MealList from './MealList';
@@ -10,13 +10,18 @@ import { spacing } from '../../theme/styles';
 import FirstOpenDialog from '../FirstOpenDialog';
 import RestaurantList from './RestaurantList';
 import messaging from '@react-native-firebase/messaging';
+import { CALENDAR_URL } from '@env';
+import HTML from 'react-native-render-html';
+import openLink from '../../Common/InAppBrowser';
+import { deviceWidth } from '../../utils/deviceHeight';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const MealSearchController = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
-
+  const [calendarEvents, setCalendarEvents] = useState(null);
+  const [showNews, setShowNews] = useState(false);
   const { t } = React.useContext(LocalizationContext);
   const styles = useStyles();
-
   const mealsByName = 0;
   const mealsByPlaces = 1;
   const mealsByDate = 2;
@@ -36,9 +41,7 @@ const MealSearchController = () => {
   };
 
   useEffect(() => {
-    /*
-    getFcmToken();
-*/
+    //  getFcmToken();
 
     let isMounted = true;
 
@@ -46,6 +49,19 @@ const MealSearchController = () => {
       database.deleteMeal();
       database.deleteRestaurant();
     }
+    fetch(CALENDAR_URL + 'lang=de&start_date=2021-09-01')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        if (data && data.events.length > 0) {
+          const eventData = data.events;
+          //   const isBetweenDates = moment().isBetween(eventData.start_date, eventData.end_date);
+          //if (isBetweenDates) {
+          setCalendarEvents(eventData);
+          //  }
+        }
+      });
+
     return () => {
       isMounted = false;
     };
@@ -54,6 +70,53 @@ const MealSearchController = () => {
   const SegmentedController = function () {
     return (
       <View style={styles.container}>
+        {calendarEvents &&
+          (!showNews ? (
+            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent:"center", height:60 }}>
+              <Icon name="newspaper-outline" style={{ paddingRight: 10 }} type="ionicon" size={25} />
+              <Button
+                buttonStyle={{ backgroundColor: 'transparent' }}
+                title={'Show news'}
+                onPress={() => setShowNews(true)}
+              />
+            </View>
+          ) : (
+            <ScrollView horizontal contentContainerStyle={{ flexDirection: 'row' }}>
+              {calendarEvents.map((events, i) => (
+                <View
+                  key={i}
+                  style={{
+                    padding: 8,
+                    backgroundColor: '#ffd420',
+                    width: deviceWidth - 50,
+                    borderRadius: 10,
+                    marginVertical: 8,
+                    marginHorizontal: 8,
+                    height: 200,
+                    justifyContent: 'space-between',
+                  }}>
+                  <Text h2>{events.title}</Text>
+                  {events.excerpt ? <HTML source={{ html: events.excerpt }} /> : null}
+                  <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <Button
+                      title={'Später'}
+                      type={'outline'}
+                      containerStyle={{ width: '40%', margin: 5 }}
+                      buttonStyle={{ paddingHorizontal: 18, backgroundColor: 'transparent' }}
+                      onPress={() => setShowNews(false)}
+                    />
+
+                    <Button
+                      title={'Mehr'}
+                      containerStyle={{ width: '40%', margin: 5 }}
+                      buttonStyle={{ backgroundColor: '#fff' }}
+                      onPress={() => openLink(events.url)}
+                    />
+                  </View>
+                </View>
+              ))}
+            </ScrollView>
+          ))}
         <SegmentedControlTab
           values={[t('Entries.Meals'), t('Entries.Places'), t('Entries.Date')]}
           accessibilityLabels={[
--- src/utils/deviceHeight.js
+++ src/utils/deviceHeight.js
@@ -1,6 +1,7 @@
 import { Dimensions, Platform, StatusBar } from 'react-native';
 
 export const deviceHeight = Dimensions.get('window').height;
+export const deviceWidth = Dimensions.get('window').width;
 export const DEVICE_HEIGHT = Platform.select({
   ios: deviceHeight,
   android: deviceHeight - StatusBar.currentHeight,
