b5c4b434d40a46e6f8b98bdd1756227ce5b113cf
Kevin <hello@kevinroehl.com>
2021-09-10 17:55:43 +0200

    news section

--- src/Screens/MealEntries/MealSearchController.js
+++ src/Screens/MealEntries/MealSearchController.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView, View } from 'react-native';
+import { View } from 'react-native';
 import LocalizationContext from '../../../LanguageContext';
 import { database } from '../../Common/database_realm';
-import { Button, makeStyles, Text } from 'react-native-elements';
+import { FAB, makeStyles } from "react-native-elements";
 import SegmentedControlTab from 'react-native-segmented-control-tab';
 import DateList from './DateList';
 import MealList from './MealList';
@@ -10,15 +10,11 @@ import { spacing } from '../../theme/styles';
 import FirstOpenDialog from '../FirstOpenDialog';
 import RestaurantList from './RestaurantList';
 import messaging from '@react-native-firebase/messaging';
-import { CALENDAR_URL } from '@env';
-import HTML from 'react-native-render-html';
-import openLink from '../../Common/InAppBrowser';
-import { deviceWidth } from '../../utils/deviceHeight';
+
+import News from './News/News';
 
 const MealSearchController = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const [calendarEvents, setCalendarEvents] = useState(null);
-  const [showNews, setShowNews] = useState(false);
   const { t } = React.useContext(LocalizationContext);
   const styles = useStyles();
   const mealsByName = 0;
@@ -48,76 +44,15 @@ const MealSearchController = () => {
       database.deleteMeal();
       database.deleteRestaurant();
     }
-    fetch(CALENDAR_URL + 'lang=de&start_date=2021-09-01')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        if (data && data.events.length > 0) {
-          const eventData = data.events;
-          //   const isBetweenDates = moment().isBetween(eventData.start_date, eventData.end_date);
-          //if (isBetweenDates) {
-          setCalendarEvents(eventData);
-          //  }
-        }
-      });
 
     return () => {
       isMounted = false;
     };
   }, []);
 
-  const SegmentedController = function () {
+  const SegmentedController = function() {
     return (
       <View style={styles.container}>
-        {calendarEvents && (
-          <ScrollView horizontal contentContainerStyle={{ flexDirection: 'row' }}>
-            {calendarEvents.map((events, i) => (
-              <View
-                key={i}
-                style={{
-                  padding: 8,
-                  backgroundColor: '#f5f5f5',
-                  width: deviceWidth - 80,
-                  borderRadius: 10,
-                  marginVertical: 8,
-                  marginHorizontal: 8,
-                  height: showNews ? 200 : 50,
-                  justifyContent: 'space-between',
-                }}>
-                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                  <Text h3 h3Style={{ fontSize: 17 }}>
-                    {events.title}
-                  </Text>
-                  {!showNews && (
-                    <Button
-                      title={'lesen'}
-                      buttonStyle={{ backgroundColor: 'transparent' }}
-                      onPress={() => setShowNews(true)}
-                    />
-                  )}
-                </View>
-                {events.excerpt && showNews ? <HTML source={{ html: events.excerpt }} /> : null}
-                {showNews && (
-                  <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <Button
-                      title={'schließen'}
-                      type={'outline'}
-                      containerStyle={{ width: '40%', margin: 5 }}
-                      buttonStyle={{ paddingHorizontal: 18, backgroundColor: 'transparent' }}
-                      onPress={() => setShowNews(false)}
-                    />
-
-                    <Button
-                      title={'mehr'}
-                      containerStyle={{ width: '40%', margin: 5 }}
-                      onPress={() => openLink(events.url)}
-                    />
-                  </View>
-                )}
-              </View>
-            ))}
-          </ScrollView>
-        )}
         <SegmentedControlTab
           values={[t('Entries.Meals'), t('Entries.Places'), t('Entries.Date')]}
           accessibilityLabels={[
@@ -142,7 +77,9 @@ const MealSearchController = () => {
     return (
       <>
         <FirstOpenDialog />
+
         <MealList controlBar={<SegmentedController />} />
+        <News />
       </>
     );
   } else if (selectedIndex === mealsByPlaces) {
--- src/Screens/MealEntries/News/News.js
+++ src/Screens/MealEntries/News/News.js
@@ -1,18 +1,105 @@
-import React from 'react';
-import { View } from 'react-native';
-import {makeStyles} from 'react-native-elements';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, View } from 'react-native';
+import { Button, makeStyles, Text } from 'react-native-elements';
+import { deviceWidth } from '../../../utils/deviceHeight';
+import HTML from 'react-native-render-html';
+import openLink from '../../../Common/InAppBrowser';
+import LocalizationContext from '../../../../LanguageContext';
+import { CALENDAR_URL } from '@env';
 
-
- const News = (props) => {
-   const {t} = React.useContext(LocalizationContext);
-   const styles = useStyles();
+const News = props => {
+  const { t } = React.useContext(LocalizationContext);
+  const styles = useStyles();
+  const [calendarEvents, setCalendarEvents] = useState(null);
+  const [showNews, setShowNews] = useState(false);
+  useEffect(() => {
+    const url = CALENDAR_URL + 'lang=de'
+    console.log(url)
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        if (data && data.events.length > 0) {
+          const eventData = data.events;
+          //   const isBetweenDates = moment().isBetween(eventData.start_date, eventData.end_date);
+          //if (isBetweenDates) {
+          setCalendarEvents(eventData);
+          //  }
+        }
+      });
+  }, []);
 
   return (
-    
-  )
+    calendarEvents && (
+      <View style={{ position: 'absolute', bottom: 0, left: 0, right: 0, backgroundColor:'#2638ff' }}>
+        {/*      {!showNews ? (
+          <FAB placement={'right'} onPress={() => setShowNews(true)} title={'news'} />
+        ) : (*/}
+        <ScrollView
+          horizontal
+          contentContainerStyle={{
+            flexDirection: 'row',
+            height: showNews ? 200 : 70,
+            justifyContent: 'space-between',
+          }}>
+          {calendarEvents.map((events, i) => (
+            <View
+              key={i}
+              style={{
+                padding: 8,
+                backgroundColor: '#f7f7f7',
+                width: deviceWidth - 80,
+                borderRadius: 10,
+                marginVertical: 8,
+                marginHorizontal: 8,
+                borderColor: '#2638ff',
+                borderStyle: 'solid',
+                borderWidth: 1,
+                justifyContent: 'space-between',
+              }}>
+              <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                <Text h3 h3Style={{ fontSize: 17 }}>
+                  {events.title}
+                </Text>
+                {!showNews && (
+                  <Button
+                    title={'lesen'}
+                    buttonStyle={{ backgroundColor: 'transparent' }}
+                    onPress={() => setShowNews(true)}
+                  />
+                )}
+              </View>
+              {events.excerpt && showNews ? <HTML source={{ html: events.excerpt }} /> : null}
+              {showNews && (
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}>
+                  <Button
+                    title={'schließen'}
+                    type={'outline'}
+                    containerStyle={{ width: '40%', margin: 5 }}
+                    buttonStyle={{ paddingHorizontal: 18, backgroundColor: 'transparent' }}
+                    onPress={() => setShowNews(false)}
+                  />
+
+                  <Button
+                    title={'mehr'}
+                    containerStyle={{ width: '40%', margin: 5 }}
+                    onPress={() => openLink(events.url)}
+                  />
+                </View>
+              )}
+            </View>
+          ))}
+        </ScrollView>
+        {/*)}*/}
+      </View>
+    )
+  );
 };
 
-export default News
+export default News;
 
-const useStyles = makeStyles(theme => ({
-}));
+const useStyles = makeStyles(theme => ({}));
