4ab60e165883d0ce1af80ebdcd782c5e07f1b78a
ppphiiil <pm@lumind-solutions.com>
2022-03-31 13:34:35 +0200

    deleted log and refactoring if statement

--- src/utils/checkAPI.js
+++ src/utils/checkAPI.js
@@ -1,7 +1,6 @@
 // checks the API varaibles in the .env file.
 export const checkAPI = (apiName, apiFromEnvFile) => {
-  console.log('checkAPI ' + apiName + ' ' + apiFromEnvFile);
-  if (apiFromEnvFile === undefined || apiFromEnvFile.length === 0) {
+  if (!apiFromEnvFile) {
     console.error(`API: ${apiName} is undefined. Define a Key in your .env file`);
   }
 };
